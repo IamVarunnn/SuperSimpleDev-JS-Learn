982fa6aff4f440cf6b57e4d834791bc09da1a703
IamVarunnn <varunrakesh16@gmail.com>
2026-05-05 20:28:30 +0530

    Large lesson

--- Javascript/Lesson - 13/Amazon-Project/data/cart.js
+++ Javascript/Lesson - 13/Amazon-Project/data/cart.js
@@ -1,15 +1,19 @@
-export let cart = [
-    {
-        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity : 1,
-        
-    },
+export let cart = JSON.parse(localStorage.getItem('cart')); 
 
-    {
-        productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity : 2,
-    }
-];
+if(!cart){
+    cart = [
+        {
+            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity : 1,
+            
+        },
+
+        {
+            productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+            quantity : 2,
+        }
+    ];
+}
 
 export function addtoCartItems(productId, quantity){
   let matchingItem;
@@ -27,6 +31,8 @@ export function addtoCartItems(productId, quantity){
         quantity,
       });
     }
+
+    saveToStorage();
 }
 
 export function removeFromCart(prodcutId){
@@ -39,5 +45,11 @@ export function removeFromCart(prodcutId){
     });
 
     cart = newCart;
+
+    saveToStorage();
 }
 
+function saveToStorage(){
+    let str = JSON.stringify(cart);
+    localStorage.setItem('cart',str);
+}
