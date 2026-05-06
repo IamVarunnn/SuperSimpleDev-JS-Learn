2f4150ddf8f7b329f2b4eac16667ec05e2c48bfa
IamVarunnn <varunrakesh16@gmail.com>
2026-05-06 14:37:55 +0530

    Doing

---
+++ Javascript/Lesson - 13/Amazon-Module-P/data/cart.js
@@ -0,0 +1,47 @@
+
+export let cart = [
+    {
+        productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity : 1
+    },
+
+    {
+        productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity : 2
+    }
+];
+
+
+export function addToCart(productId, quantity){
+    let matchingItem;
+    
+    cart.forEach((item)=>{
+        if(productId === item.productId){
+            matchingItem = item;
+        }
+    });
+
+
+    if(matchingItem){
+        matchingItem.quantity += quantity;
+    }
+    else{
+        cart.push({
+            productId,
+            quantity,
+        });
+    }
+    
+}
+
+export function showQuantity(){
+    let cartQuantity = 0;
+
+    cart.forEach((item)=>{
+        cartQuantity += item.quantity;
+    });
+
+    let cartQuantityCount = document.querySelector('.cart-quantity');
+    cartQuantityCount.innerText = cartQuantity;
+    
+}
